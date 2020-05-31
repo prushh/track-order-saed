84dbf5dbdfe0791f1b26f2d1caab2ef68835b928
Amir Ouakri <amir.ouakri@gmail.com>
2020-05-31 15:17:23 +0200

    insert debugging function

--- public/utils.php
+++ public/utils.php
@@ -7,3 +7,56 @@
  */
 
 //Utility functions
+
+// ** DEBUGGING ** //
+
+/**
+ * metodo che effettua il log di una variabile per il debug
+ *
+ * @param mixed $var variabile da stampare
+ * @param boolean $die flag indicante se il programma va arrestato
+ */
+
+function _debug_log($var,$die = true) {
+
+    print('<pre>' . print_r($var,true) . '</pre>');
+
+    if($die)
+
+        die;
+
+}
+
+/**
+ * metodo che effettua l'output di un testo per il debug
+ *
+ * @param string $var testo da stampare
+ * @param boolean $die flag indicante se il programma va arrestato
+ */
+
+function _debug_echo($var,$die = true) {
+
+    echo $var . PHP_EOL;
+
+    if($die)
+
+        die;
+
+}
+
+/**
+ * metodo che effettua il dump di una variabile
+ *
+ * @param mixed $var variabile su cui fare il dumping
+ * @param boolean $die flag indicante se il programma va arrestato
+ */
+
+function _debug_dump($var,$die = true) {
+
+    var_dump($var);
+
+    if($die)
+
+        die;
+
+}
